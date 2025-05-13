a2e1e8f6f07dabc9a8923e3f154ab5d18e09fcd5
ByNetSSer <nasser.gonzales@tecsup.edu.pe>
2025-05-13 10:53:43 -0500

    G2 Movement Done

---
+++ Assets/Scripts/Game 2/CharacterControler3D.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CharacterControler3D : MonoBehaviour
+{
+    [SerializeField] private Rigidbody MyRGBD;
+    [SerializeField] private float Speed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private Vector3 Vector;
+    private void Awake()
+    {
+        MyRGBD = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 forcemovement = new Vector3( Vector.x,0,Vector.z);
+        //MyRGBD.linearVelocity = forcemovement * Speed;
+        MyRGBD.AddForce(forcemovement * Speed);
+    }
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector = new Vector3(context.ReadValue<Vector2>().x,0 , context.ReadValue<Vector2>().y);
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        MyRGBD.AddForce(Vector3.up * JumpForce);
+    }
+    public void ApplyPhysics()
+    {
+
+    }
+}
