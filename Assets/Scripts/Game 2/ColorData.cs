a1d389da894ff7cdd272aa277aaf26f19c2d46f2
ByNetSSer <nasser.gonzales@tecsup.edu.pe>
2025-05-13 20:50:10 -0500

    G2 Start Color Mechanic

---
+++ Assets/Scripts/Game 2/ColorData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Color Data",menuName ="Scriptable Objects/Game 2/ColorData")]
+public class ColorData : ScriptableObject
+{
+    public Color color;
+    
+}
---
+++ Assets/Scripts/Game 2/ColorPropety.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ColorPropety : MonoBehaviour
+{
+    [SerializeField] protected ColorData colorData;
+    [SerializeField] protected MeshRenderer meshRenderer;
+    protected Material _material;
+    private void Awake()
+    {
+       meshRenderer = GetComponent<MeshRenderer>();
+        
+    }
+    protected void Start()
+    {
+        _material = meshRenderer.material;
+
+        SetUpColor(colorData);
+    }
+    protected void SetUpColor(ColorData newColorData)
+    {
+        colorData = newColorData;
+
+        _material.SetColor("_Color", colorData.color);
+    }
+}
